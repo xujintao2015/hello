89ad09744f5baf7241d209c6f5fafe382bc55aef
xujintao2015 <xujintao2012@qq.com>
2017-06-07 19:04:17 +0800

    hellocontroller

--- hello/src/main/java/hello/HelloController.java
+++ hello/src/main/java/hello/HelloController.java
@@ -1,19 +1,27 @@
 package hello;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@RestController
+@EnableDiscoveryClient
 public class HelloController {
+	
+	@Value("${hello}")
+	 private String  hello="hello";
 
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return "Hello World!" + " "+hello;
     }
 
     public static void main(String[] args) throws Exception {
